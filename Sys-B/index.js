1835b87d5ef9a4e3ee385865bea5d7bc495e58c5
daniellisachuk <daniellisachuk@gmail.com>
2020-08-29 03:58:39 +0300

    added kafka to websocket (pre reddis)

--- Sys-B/index.js
+++ Sys-B/index.js
@@ -17,14 +17,41 @@ app.set("view engine", 'hbs');
 
 app.use('/', router);
 
-const kafka = require('./kafka/KafkaConsume');
+// Reddis
+// const reddis = require(/* path to reddis uploader file */)
 
 // WebSocket
 io.on("connection", (socket) => {
-    console.log("new user connected");
-    // send data to client side upon arrival of kafka data
+    console.log(`----> New User Connected : ${socket.id}`);
+    socket.on("disconnect", () => {
+      console.log(`<---- User Disconnected : ${socket.id}`);
+    })
 });
 
+// Kafka
+var counter = 0;
+const kafka = require('./kafka/kafkaConsume');
+kafka.on("data", function(m) {
+  counter++;
+  // commit offset every 5 messages
+  if (counter % 5 === 0) {
+    console.log("calling commit");
+    kafka.commit(m);
+  }
+
+  jsonMessage = JSON.parse(m.value);
+  console.log(`got message:\n${jsonMessage.toString()}`);
+  // console.log(m.value.toString());
+
+  // reddis.store(m);
+
+  // will send data to all client side sockets upon arrival of kafka data
+  io.emit("callDetails", jsonMessage);
+
+});
+kafka.connect();
+
+
 server.listen(port, () => {
-  console.log("Listening on port " + port);
+  console.log("HTTP Monitoring service - System B - Running\nListening for connections on port " + port);
 });
